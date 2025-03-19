9112b9b3ec8a925efc129675e5fbaa57dfc50a64
vorobyevay <vay@tract.ru>
2025-03-19 12:04:58 +0300

    Запилил сапера

--- C Projects/saper.c
+++ C Projects/saper.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main()
 {
-    int x = 0;
-    printf("Enter your num from 0 to 127\n");
-    scanf("%d", &x);
-    printf("You entered %d", x);
+    srand(time(NULL));
+
+    int x;
+    int ind = 0;
+    int arr1[5][5];
+    int arr2[5][5];
+    
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            arr1[i][j] = rand() % 128;
+            arr2[i][j] = 0;
+            printf("%d ", arr1[i][j]);
+        }
+        printf("\n");
+    }
+
+    while(ind < 25) {
+        printf("Enter your num from 0 to 127\n");
+        scanf("%d", &x);
+    
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr1[i][j] == x){
+                    printf("%d ", x);
+                    arr2[i][j] = 1;
+                    ind++;
+                } else if (arr2[i][j] == 1){
+                    printf("%d ", arr1[i][j]);
+                } else {
+                    printf("[x]");
+               }
+            }
+            printf("\n");
+        }
+    }
+
+    printf("YOU WIN!");
+
+    //printf("You entered %d", x);
     return 0;
 }
