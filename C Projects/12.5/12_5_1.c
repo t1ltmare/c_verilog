85920df06d4d06c9f7a2a48bf5b68263343d5ca1
vorobyevay <vay@tract.ru>
2025-03-26 17:50:38 +0300

    Правил 12_5_1

---
+++ C Projects/12.5/12_5_1.c
@@ -0,0 +1,21 @@
+// TODO С помощью malloc, calloc, free выделить память для динамического массива, размер которого вводится из консоли
+// TODO Массив заполняется из консоли
+// TODO Полученный массив выводится в консоль поэлементно в строку
+// TODO После вывода массива необходимо освободить память, далее заново попробовать вывести этот массив
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int size;
+    int *array;
+  
+    // 1. Input array size from console
+    printf("Enter array size: ");
+    scanf("%d", &size);
+
+    // 2. Allocate memory for array
+    
+  
+    return 0;
+  }
