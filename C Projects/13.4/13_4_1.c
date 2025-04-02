dfe44a88002680bc8f769f6b4a7fe98a262eea52
vorobyevay <vay@tract.ru>
2025-04-02 15:00:39 +0300

    Выполнил 13.4

--- C Projects/13.4/13_4_1.c
+++ C Projects/13.4/13_4_1.c
@@ -3,9 +3,67 @@
 #include <math.h>
 #include <time.h>
 
-//TODO Функция выделения памяти для массива (размер вводится с консоли)
-//TODO Заполнение массива синусоидальным сигналом
-//TODO Дополнение сигнала шумовой составляющей (+-5%)
-//TODO Вывод полученного массива чисел
+// Function to allocate memory for an array
+void memAlloc(double **p, int n)
+{
+    *p = malloc(n * sizeof(double));
+    if (*p == NULL)
+    {
+        printf("Error: Memory allocation failed.\n");
+        exit(1);
+    }
+}
 
-//TODO *** Нарисовать график сигнала до и после добавления шума ***
+// Function to print an array
+void printArr(double *p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%.2f ", p[i]);
+    }
+}
+
+// Function to fill an array with sine values
+void fillArr(double *p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        p[i] = sin(i);
+    }
+}
+
+void addNoise(double *p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        double variation = (double)(rand() % 10 - 5) / 100.0; 
+        p[i] += variation;
+    }
+}
+
+int main()
+{
+    printf("Enter the array size\n");
+    int size;
+    if (scanf("%d", &size) != 1 || size <= 0)
+    {
+        printf("Error: Invalid array size input.\n");
+        return 1;
+    }
+
+    double *arr = NULL;
+
+    srand(time(NULL));
+
+    memAlloc(&arr, size);
+    fillArr(arr, size);
+    printArr(arr, size);
+    printf("\n");
+    addNoise(arr, size);
+    printArr(arr, size);
+
+    // Free the allocated memory
+    free(arr);
+
+    return 0;
+}
