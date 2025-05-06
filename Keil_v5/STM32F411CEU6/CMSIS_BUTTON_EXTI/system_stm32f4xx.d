71460c97e2702affeeb2997611f1289b22b834c3
vorobyevay <artem.vorobyov1@yandex.ru>
2025-05-06 11:20:17 +0300

    Выполняю 6.5

--- Keil_v5/STM32F411CEU6/CMSIS_BUTTON_EXTI/system_stm32f4xx.d
+++ Keil_v5/STM32F411CEU6/CMSIS_BUTTON_EXTI/system_stm32f4xx.d
@@ -1,11 +1,10 @@
-./system_stm32f4xx.o: RTE/Device/STM32F411CEUx/system_stm32f4xx.c \
-  C:/Users/vorobyevay/AppData/Local/Keil_v5_2/ARM/PACK/Keil/STM32F4xx_DFP/2.11.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include\stm32f4xx.h \
-  C:/Users/vorobyevay/AppData/Local/Keil_v5_2/ARM/PACK/Keil/STM32F4xx_DFP/2.11.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
-  C:/Users/vorobyevay/AppData/Local/Keil_v5_2/ARM/PACK/ARM/CMSIS/5.1.1/CMSIS/Include\core_cm4.h \
-  C:\Users\vorobyevay\AppData\Local\Keil_v5_2\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:/Users/vorobyevay/AppData/Local/Keil_v5_2/ARM/PACK/ARM/CMSIS/5.1.1/CMSIS/Include/cmsis_version.h \
-  C:/Users/vorobyevay/AppData/Local/Keil_v5_2/ARM/PACK/ARM/CMSIS/5.1.1/CMSIS/Include/cmsis_compiler.h \
-  C:/Users/vorobyevay/AppData/Local/Keil_v5_2/ARM/PACK/ARM/CMSIS/5.1.1/CMSIS/Include/cmsis_armclang.h \
-  C:\Users\vorobyevay\AppData\Local\Keil_v5_2\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
-  C:/Users/vorobyevay/AppData/Local/Keil_v5_2/ARM/PACK/ARM/CMSIS/5.1.1/CMSIS/Include/mpu_armv7.h \
-  C:/Users/vorobyevay/AppData/Local/Keil_v5_2/ARM/PACK/Keil/STM32F4xx_DFP/2.11.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+./system_stm32f4xx.o: RTE\Device\STM32F411CEUx\system_stm32f4xx.c \
+  C:\Users\justd\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
+  C:\Users\justd\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f411xe.h \
+  C:\Users\justd\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\justd\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\justd\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\justd\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\justd\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\justd\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
--- Keil_v5/STM32F411CEU6/CMSIS_PWM_LED/main.c
+++ Keil_v5/STM32F411CEU6/CMSIS_PWM_LED/main.c
@@ -1,80 +1,103 @@
 #include "stm32f4xx.h"
 #include "stdint.h"
 
-#define LEDPIN 13
-#define BUTPIN 0 
+#define BUTTON_PORT GPIOA
+#define BUTTON_PIN 0
+#define PWM_PORT GPIOB
+#define PWM_PIN 6
+#define PWM_AF 2 // AF2 для TIM4_CH1 на PB6
 
-volatile uint32_t duty_cycle_index = 0;
-const uint32_t duty_cycles[] = {0, 20, 40, 60, 80, 100};
-const uint32_t num_duty_cycles = sizeof(duty_cycles) / sizeof(duty_cycles[0]);
+volatile uint8_t dutyCycleIndex = 0;
+const uint8_t dutyCycles[] = {0, 20, 40, 60, 80, 100};
+const uint8_t numDutyCycles = sizeof(dutyCycles) / sizeof(dutyCycles[0]);
+volatile uint32_t lastButtonPress = 0;                 
 
-void led_init(void) 
-{
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-    
-    GPIOC->MODER &= ~(0x3UL << (LEDPIN * 2)); 
-    GPIOC->MODER |= (0x1UL << (LEDPIN * 2));
-    
-    GPIOC->AFR[0] &= ~(0xF << (LEDPIN * 4));  // Сбросить биты AFR для пина
-    GPIOC->AFR[0] |= (0x1 << (LEDPIN * 4));  // Выбрать AF1 (TIM2_CH1)
+void GPIO_Config(void) {
+  // 1. Включаем тактирование портов GPIOA и GPIOB
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN;
+
+  // 2. Настройка пина кнопки (GPIOA Pin 0)
+  GPIOA->MODER &= ~(0x03 << (BUTTON_PIN * 2)); // Сбрасываем биты MODER
+  GPIOA->PUPDR |= (0x01 << (BUTTON_PIN * 2));  // Подтягивающий резистор
+
+  // 3. Настройка пина ШИМ (GPIOB Pin 6) как альтернативную функцию
+  GPIOB->MODER &= ~(0x03 << (PWM_PIN * 2)); // Сбрасываем биты MODER
+  GPIOB->MODER |= (0x02 << (PWM_PIN * 2));  // Альтернативная функция
+  GPIOB->OTYPER &= ~(1 << PWM_PIN);          // Push-pull выход
+  GPIOB->OSPEEDR |= (0x03 << (PWM_PIN * 2)); // Высокая скорость
+  GPIOB->PUPDR &= ~(0x03 << (PWM_PIN * 2));  // Без подтягивающих резисторов
+
+  // 4. Выбор альтернативной функции
+  if (PWM_PIN <= 7) {
+    GPIOB->AFR[0] &= ~(0x0F << (PWM_PIN * 4));
+    GPIOB->AFR[0] |= (PWM_AF << (PWM_PIN * 4));
+  } else {
+    GPIOB->AFR[1] &= ~(0x0F << ((PWM_PIN - 8) * 4));
+    GPIOB->AFR[1] |= (PWM_AF << ((PWM_PIN - 8) * 4));
+  }
 }
 
-void button_init(void)
-{
-    RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Тактирование порта кнопки
-    
-    GPIOA -> MODER &= ~(0x03UL << (0 * 2)); // Сброс битов режима для пина
-    GPIOA -> MODER |= (0x00UL << (0 * 2)); // Установка пина на вход
-    
-    GPIOA -> PUPDR &= ~(0x03UL << (0 * 2)); // Сброс PU PD
-    GPIOA -> PUPDR |= (0x01UL << (0 * 2)); // Включить pull-up
-    
-    RCC -> APB2ENR |= RCC_APB2ENR_SYSCFGEN; // Включить тактирование SYSCFG
-    
-    SYSCFG -> EXTICR[BUTPIN / 4] &= ~(0x0FUL << ((BUTPIN % 4) * 4)); // Сброс SYSCFG
-    SYSCFG -> EXTICR[BUTPIN / 4] |= (0x00UL << ((BUTPIN % 4) * 4)); // Выбор порта GPIOA
-    
-    EXTI -> IMR |= (0x01UL << BUTPIN); // Разрешаем генерацию прерывания EXTI0
-    EXTI -> FTSR |= (0x01UL << BUTPIN); // Прерывание по спаду
-    
-    NVIC_EnableIRQ(EXTI0_IRQn); // Разрешаем прерывание в NVIC
-    NVIC_SetPriority(EXTI0_IRQn, 0); // Устанавливаем высокий приоритет прерывания
+// Функция для инициализации ШИМ (Таймер 4)
+void TIM_Config(void) {
+  RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+  TIM4->PSC = 99;
+  TIM4->ARR = 999;
+
+  TIM4->CCMR1 |= (TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1PE); 
+  TIM4->CCER |= TIM_CCER_CC1E;                                           
+
+  TIM4->CR1 |= TIM_CR1_ARPE | TIM_CR1_CEN;
 }
 
-void pwm_init(void) 
-{
-    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-    
-    TIM2->PSC = 999;  
-    TIM2->ARR = 99;
+// Функция для установки коэффициента заполнения ШИМ
+void setDutyCycle(uint8_t dutyCyclePercentage) {
+  // Преобразуем процент в значение CCR (Capture/Compare Register)
+  uint16_t pulseWidth = (uint16_t)((dutyCyclePercentage * (TIM4->ARR + 1)) / 100);
 
-    TIM2->CCMR1 &= ~TIM_CCMR1_CC1S;  
-    TIM2->CCMR1 |= (TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1); 
-    TIM2->CCMR1 |= TIM_CCMR1_OC1PE;  
-    TIM2->CCER |= TIM_CCER_CC1E;    
-
-    TIM2->CCR1 = 0;
-
-    TIM2->CR1 |= TIM_CR1_CEN;
-    TIM2->EGR |= TIM_EGR_UG;
+  // Устанавливаем значение CCR для канала 1
+  TIM4->CCR1 = pulseWidth;
 }
 
-void EXTI0_IRQHandler(void) 
-{
-    if (EXTI->PR & (0x1UL << BUTPIN)) {
-        EXTI->PR |= (0x1UL << BUTPIN);
-        duty_cycle_index = (duty_cycle_index + 1) % num_duty_cycles;
-        TIM2->CCR1 = (duty_cycles[duty_cycle_index] * TIM2->ARR) / 100; 
-    }
+volatile uint32_t sysTickCounter = 0; // volatile ОЧЕНЬ ВАЖНО!
+
+void SysTick_Handler(void) { // Имя функции SysTick_Handler ОЧЕНЬ ВАЖНО!
+  sysTickCounter++;
+}
+
+uint32_t HAL_GetTick(void) { // Тип возвращаемого значения uint32_t ОЧЕНЬ ВАЖНО!
+  return sysTickCounter;
 }
 
 int main(void) {
-    led_init(); 
-    pwm_init();
-    button_init();
+  // 1. Инициализация периферии
+  GPIO_Config();
+  TIM_Config();
 
-    while (1) 
-    {
-        
+  // 2. Начальная установка ШИМ
+  setDutyCycle(dutyCycles[dutyCycleIndex]);
+
+  // Настройка SysTick для HAL_GetTick() (нужно для устранения дребезга)
+  SystemCoreClockUpdate();
+  SysTick_Config(SystemCoreClock / 300); // 1ms tick
+
+  while (1) {
+    // 3. Чтение состояния кнопки
+    if (!(GPIOA->IDR & (1 << BUTTON_PIN))) { // Кнопка нажата (предполагается pull-up)
+      // 4. Устранение дребезга
+      if ((HAL_GetTick() - lastButtonPress) > 50) {
+        // 5. Увеличение индекса коэффициента заполнения
+        dutyCycleIndex++;
+        if (dutyCycleIndex >= numDutyCycles) {
+          dutyCycleIndex = 0; // Вернуться к началу, если достигнут конец массива
+        }
+
+        // 6. Установка нового коэффициента заполнения
+        setDutyCycle(dutyCycles[dutyCycleIndex]);
+
+        // 7. Запоминание времени последнего нажатия кнопки
+        lastButtonPress = HAL_GetTick();
+      }
     }
+  }
 }
+
